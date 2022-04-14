7c88376fb0d1a918c43e724053cf139820c96961
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-14 11:28:24 +0900

    feat: add opensource api

---
+++ pages/api/opensource.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface Project {
+  projectName: string;
+  projectURL: string;
+  contributeType: string; // 기여 종류 (번역, 코드 수정 등등)
+  prURL: Array<string>; // Pull Request URL
+}
+
+type OpenSource = Array<Project>;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const openSource: OpenSource = [
+    {
+      projectName: "tldr",
+      projectURL: "https://github.com/tldr-pages/tldr",
+      contributeType: "번역",
+      prURL: [
+        "https://github.com/tldr-pages/tldr/pull/7959",
+        "https://github.com/tldr-pages/tldr/pull/7960",
+        "https://github.com/tldr-pages/tldr/pull/7970",
+      ],
+    },
+  ];
+  res.status(200).json(openSource);
+}
