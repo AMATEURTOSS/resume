76d3b5cdc994e6f21b82ca7af977de5dddbb27e8
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-14 16:07:37 +0900

    feat: add basic info section

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
-import type { NextPage } from "next";
+import { getData } from "./api/whoami";
+import type { NextPage, GetStaticProps } from "next";
+import type { WhoAmI } from "./api/whoami";
 
-const Home: NextPage = () => {
-  return <div></div>;
+const Home: NextPage<{ whoami: WhoAmI }> = ({ whoami }) => {
+  return (
+    <main>
+      <section className="basic_info">
+        <img src="profile.jpg" alt="profile" />
+        <div>
+          <h1 className="name">{whoami.name}</h1>
+          <span className="email">
+            <i className="xi-mail-o" />
+            {whoami.email}
+          </span>
+          <div className="phone_birth_year">
+            <span>
+              <i className="xi-call" />
+              {whoami.phone}
+            </span>
+            <span>
+              <i className="xi-calendar" />
+              {whoami.birth}
+            </span>
+            <span>
+              <i className="xi-briefcase" />
+              {whoami.year}년차
+            </span>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export const getStaticProps: GetStaticProps<{ whoami: WhoAmI }> = async () => {
+  const whoami: WhoAmI = await getData();
+  return {
+    props: {
+      whoami,
+    },
+  };
 };
 
 export default Home;
