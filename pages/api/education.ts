de24719b893d3eec0ac6efba0dcb68a26d97530a
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-15 11:38:16 +0900

    feat: add education section

--- pages/api/education.ts
+++ pages/api/education.ts
@@ -4,8 +4,8 @@ interface School {
   name: string; // 학교 이름
   type: string; // 학교 종류 (고등학교, 4년제 대학, 비학위 등등)
   department: string | undefined; // 학과
-  admissionDate: Date; // 입학일자
-  graduationDate: Date | undefined; // 졸업일자
+  admissionDate: string; // 입학일자
+  graduationDate: string | undefined; // 졸업일자
   describe: string | undefined;
 }
 
@@ -17,15 +17,15 @@ export const getData = async (): Promise<Education> => {
       name: "충훈고등학교",
       type: "고등학교",
       department: "문과",
-      admissionDate: new Date("2017-03-02"),
-      graduationDate: new Date("2020-01-07"),
-      describe: undefined,
+      admissionDate: "2017-03-02",
+      graduationDate: "2020-01-07",
+      describe: "공립 인문계 고등학교",
     },
     {
       name: "42Seoul",
       type: "비학위 과정",
       department: undefined,
-      admissionDate: new Date("2020-02-24"),
+      admissionDate: "2020-02-24",
       graduationDate: undefined,
       describe: `PBL(Project Base Learning)과 동료학습(Peer Learning) 기반의 소프트웨어 인재 양성 프로그램`,
     },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import { getData as whoAmIGetData } from "./api/whoami";
 import { getData as techStackGetData } from "./api/techstack";
+import { getData as educationGetData } from "./api/education";
 import type { NextPage, GetStaticProps } from "next";
 import type { WhoAmI } from "./api/whoami";
 import type { TechStack } from "./api/techstack";
+import type { Education } from "./api/education";
 
 interface Props {
   whoami: WhoAmI;
   techStack: TechStack;
+  edu: Education;
 }
 
-const Home: NextPage<Props> = ({ whoami, techStack }) => {
+const Home: NextPage<Props> = ({ whoami, techStack, edu }) => {
   return (
     <main>
       <section className="basic_info">
@@ -44,6 +47,42 @@ const Home: NextPage<Props> = ({ whoami, techStack }) => {
           ))}
         </div>
       </section>
+      <section className="education">
+        <h2>교육 이력</h2>
+        {edu
+          .slice(0)
+          .reverse()
+          .map((el, idx) => {
+            return (
+              <div className="school-wrapper" key={idx}>
+                <div className="key_value name">
+                  <span>학교명</span>
+                  <span>{el.name}</span>
+                </div>
+                <div className="key_value type">
+                  <span>학교 종류</span>
+                  <span>{el.type}</span>
+                </div>
+                <div className="key_value admission">
+                  <span>입학일</span>
+                  <span>{el.admissionDate}</span>
+                </div>
+                <div className="key_value graduation">
+                  <span>졸업일</span>
+                  <span>{el.graduationDate ?? "재학중"}</span>
+                </div>
+                <div className="key_value department">
+                  <span>학과</span>
+                  <span>{el.department ?? "N/A"}</span>
+                </div>
+                <div className="key_value describe">
+                  <span>설명</span>
+                  <span>{el.describe ?? "N/A"}</span>
+                </div>
+              </div>
+            );
+          })}
+      </section>
     </main>
   );
 };
@@ -51,11 +90,15 @@ const Home: NextPage<Props> = ({ whoami, techStack }) => {
 export const getStaticProps: GetStaticProps<{ whoami: WhoAmI }> = async () => {
   const whoami: WhoAmI = await whoAmIGetData();
   const techStack: TechStack = await techStackGetData();
+  const edu: Education = await educationGetData();
   return {
-    props: {
-      whoami,
-      techStack,
-    },
+    props: JSON.parse(
+      JSON.stringify({
+        whoami,
+        techStack,
+        edu,
+      })
+    ),
   };
 };
 
