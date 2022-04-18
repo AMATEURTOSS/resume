8dcabd3beb15586333099252fa4f456b3f573f58
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-18 12:12:49 +0900

    feat: disable about me

--- component/About.tsx
+++ component/About.tsx
@@ -5,25 +5,25 @@ import styles from "../styles/component/About.module.css";
 const About: FC = () => {
   return (
     <div className={styles.aboutSection}>
-      <div className="container">
-        <section>
-          <h2 className={share.heading}>About Me</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            interdum ante a porta blandit. Quisque velit tortor, ultricies in
-            quam ut, luctus convallis metus. Nunc vitae felis rutrum, mattis
-            justo quis, bibendum tellus. Integer bibendum eget dolor nec
-            venenatis. Aliquam erat volutpat. Etiam et est vitae turpis accumsan
-            vulputate. Curabitur et vehicula nisl, eu congue dolor. Vivamus nec
-            hendrerit mi Nulla consequat tempor.
-          </p>
-          {/*<div className={`${styles.buttonSec} text-center`}>*/}
-          {/*  <a href="#" className={share.button}>*/}
-          {/*    Download Resume*/}
-          {/*  </a>*/}
-          {/*</div>*/}
-        </section>
-      </div>
+      {/*<div className="container">*/}
+      {/*  <section>*/}
+      {/*    <h2 className={share.heading}>About Me</h2>*/}
+      {/*    <p>*/}
+      {/*      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis*/}
+      {/*      interdum ante a porta blandit. Quisque velit tortor, ultricies in*/}
+      {/*      quam ut, luctus convallis metus. Nunc vitae felis rutrum, mattis*/}
+      {/*      justo quis, bibendum tellus. Integer bibendum eget dolor nec*/}
+      {/*      venenatis. Aliquam erat volutpat. Etiam et est vitae turpis accumsan*/}
+      {/*      vulputate. Curabitur et vehicula nisl, eu congue dolor. Vivamus nec*/}
+      {/*      hendrerit mi Nulla consequat tempor.*/}
+      {/*    </p>*/}
+      {/*    /!*<div className={`${styles.buttonSec} text-center`}>*!/*/}
+      {/*    /!*  <a href="#" className={share.button}>*!/*/}
+      {/*    /!*    Download Resume*!/*/}
+      {/*    /!*  </a>*!/*/}
+      {/*    /!*</div>*!/*/}
+      {/*  </section>*/}
+      {/*</div>*/}
     </div>
   );
 };
