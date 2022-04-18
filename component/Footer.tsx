c7ec6173fe99dcb0d96df90ff98965a3d82f7d66
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-18 11:36:42 +0900

    feat: update footer

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -8,19 +8,18 @@ const Footer: FC = () => {
       <div className="container">
         <section>
           <span>Stay in touch</span>
-          <h4>Ready to talk?</h4>
-          <p>Feel free to contact us</p>
-          <a href="mailto:test@test.com" className={share.button}>
-            Lets Talk
+          <h4>이야기를 하고싶으신가요?</h4>
+          <p>언제든지 연락주세요</p>
+          <a
+            href="mailto:amateur.toss@gmail.com"
+            className={share.button}
+            target="_blank"
+            rel="noreferrer"
+          >
+            연락하기
           </a>
         </section>
-        <span className={styles.copyright}>
-          © 2022 All rights reserved. Free minimal bootstrap template by{" "}
-          <a href="https://designstub.com/" target="_blank" rel="noreferrer">
-            Designstub
-          </a>
-          .
-        </span>
+        <span className={styles.copyright}>© 2022 All rights reserved.</span>
       </div>
     </footer>
   );
