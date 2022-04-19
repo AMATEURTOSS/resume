e7978fc9363f08353f496dad61dcad2389fa78a3
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-19 11:13:50 +0900

    feat: change techstack api feat as i18n

--- pages/api/techstack.ts
+++ pages/api/techstack.ts
@@ -1,25 +1,43 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { i18nType } from "../../i18nType";
 
 export type TechStack = {
   coreStack: Array<string>;
   otherStack: Array<string>;
 };
 
-export const getData = async (): Promise<TechStack> => {
-  const techStack: TechStack = {
-    coreStack: [
-      "javascript",
-      "typescript",
-      "nodejs",
-      "mariadb",
-      "react",
-      "expressjs",
-      "nextjs",
-      "docker",
-      "docker compose",
-      "git",
-    ],
-    otherStack: ["c", "c++", "nestjs", "python"],
+export const getData = async (): Promise<i18nType<TechStack>> => {
+  const techStack: i18nType<TechStack> = {
+    kr: {
+      coreStack: [
+        "javascript",
+        "typescript",
+        "nodejs",
+        "mariadb",
+        "react",
+        "expressjs",
+        "nextjs",
+        "docker",
+        "docker compose",
+        "git",
+      ],
+      otherStack: ["c", "c++", "nestjs", "python"],
+    },
+    "en-US": {
+      coreStack: [
+        "javascript",
+        "typescript",
+        "nodejs",
+        "mariadb",
+        "react",
+        "expressjs",
+        "nextjs",
+        "docker",
+        "docker compose",
+        "git",
+      ],
+      otherStack: ["c", "c++", "nestjs", "python"],
+    },
   };
   return techStack;
 };
