7f58e73c6e898cddb3123d2be476c47807a785b0
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-19 10:30:00 +0900

    feat: change whoami api feat as i18n

--- pages/api/whoami.ts
+++ pages/api/whoami.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { getData as projectGetData } from "./project";
 import { getData as opensourceGetData } from "./opensource";
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { i18nType } from "../../i18nType";
 
 export interface WhoAmI {
   name: string;
@@ -30,30 +31,45 @@ async function getOpensourceCount() {
   return total;
 }
 
-export const getData = async (): Promise<WhoAmI> => {
+export const getData = async (): Promise<i18nType<WhoAmI>> => {
   const projectCount = (await projectGetData()).length;
   const opensourceCount = await getOpensourceCount();
   const currierStart = new Date("2022-02-21");
   const today = new Date();
-  const whoAmI: WhoAmI = {
-    name: "최영진",
-    email: "amateur.toss@gmail.com",
-    phone: "01092124699",
-    birth: "2001-09-06",
-    year:
-      Math.abs(
-        new Date(today.getTime() - currierStart.getTime()).getUTCFullYear() -
-          1970
-      ) + 1,
-    projectCount,
-    opensourceCount,
+  const whoAmI: i18nType<WhoAmI> = {
+    kr: {
+      name: "최영진",
+      email: "amateur.toss@gmail.com",
+      phone: "01092124699",
+      birth: "2001-09-06",
+      year:
+        Math.abs(
+          new Date(today.getTime() - currierStart.getTime()).getUTCFullYear() -
+            1970
+        ) + 1,
+      projectCount,
+      opensourceCount,
+    },
+    "en-US": {
+      name: "Young-Jin Choi",
+      email: "amateur.toss@gmail.com",
+      phone: "01092124699",
+      birth: "2001-09-06",
+      year:
+        Math.abs(
+          new Date(today.getTime() - currierStart.getTime()).getUTCFullYear() -
+            1970
+        ) + 1,
+      projectCount,
+      opensourceCount,
+    },
   };
   return whoAmI;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<WhoAmI>
+  res: NextApiResponse<i18nType<WhoAmI>>
 ) {
   const whoAmI = await getData();
   res.status(200).json(whoAmI);
