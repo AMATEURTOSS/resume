236ea762ad1272bdfc7fd64ca091e7e4f688c38d
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-22 14:23:58 +0900

    build: load bootstrap from local

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,17 +23,6 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
         />
-        {/*Bootstrap CSS*/}
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
