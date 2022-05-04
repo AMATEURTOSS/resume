5591a863c60c5e472c613206f22c4dd304cb0f54
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-04 14:26:07 +0900

    feat: add project sort

--- component/Projects.tsx
+++ component/Projects.tsx
@@ -20,57 +20,60 @@ const Projects: FC<ProjectsProps> = ({ projects }) => {
       <Container>
         <h2 className={share.heading}>Personal Projects</h2>
         <div className={styles.listSection}>
-          {projects[_locale].slice(0, 4).map((project, idx) => {
-            const img = project.image[0] ?? "";
-            const startDate = new Date(project.start);
-            const endDate = new Date(project.end);
-            return (
-              <div className={styles.list} key={idx} id="project">
-                <Row>
-                  <Col sm={12} md={7} lg={7} xl={7} xxl={7}>
-                    <section>
-                      <span>
-                        {startDate.getFullYear()}.{startDate.getMonth() + 1} -{" "}
-                        {endDate.getFullYear()}.{endDate.getMonth() + 1}
-                      </span>
-                      <h3 className={styles.title}>{project.name}</h3>
-                      <ul className={styles.stack}>
-                        {project.techStack.map((stack, idx) => {
-                          const icon = Utils.getIconFromTechStack(stack);
-                          if (icon)
-                            return (
-                              <OverlayTrigger
-                                key={idx}
-                                placement="top"
-                                overlay={<Tooltip>{stack}</Tooltip>}
-                              >
-                                <li>
-                                  <i className={icon} />
-                                </li>
-                              </OverlayTrigger>
-                            );
-                        })}
-                      </ul>
-                      <p>{project.describe} </p>
-                      <a
-                        href={project.githubURL}
-                        className={share.button}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Details
-                      </a>
-                    </section>
-                  </Col>
-                  <Col sm={12} md={5} lg={5} xl={5} xxl={5}>
-                    <figure>
-                      <img src={img} />
-                    </figure>
-                  </Col>
-                </Row>
-              </div>
-            );
-          })}
+          {projects[_locale]
+            .sort((a, b) => a.sort - b.sort)
+            .slice(0, 5)
+            .map((project, idx) => {
+              const img = project.image[0] ?? "";
+              const startDate = new Date(project.start);
+              const endDate = new Date(project.end);
+              return (
+                <div className={styles.list} key={idx} id="project">
+                  <Row>
+                    <Col sm={12} md={7} lg={7} xl={7} xxl={7}>
+                      <section>
+                        <span>
+                          {startDate.getFullYear()}.{startDate.getMonth() + 1} -{" "}
+                          {endDate.getFullYear()}.{endDate.getMonth() + 1}
+                        </span>
+                        <h3 className={styles.title}>{project.name}</h3>
+                        <ul className={styles.stack}>
+                          {project.techStack.map((stack, idx) => {
+                            const icon = Utils.getIconFromTechStack(stack);
+                            if (icon)
+                              return (
+                                <OverlayTrigger
+                                  key={idx}
+                                  placement="top"
+                                  overlay={<Tooltip>{stack}</Tooltip>}
+                                >
+                                  <li>
+                                    <i className={icon} />
+                                  </li>
+                                </OverlayTrigger>
+                              );
+                          })}
+                        </ul>
+                        <p>{project.describe} </p>
+                        <a
+                          href={project.githubURL}
+                          className={share.button}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Details
+                        </a>
+                      </section>
+                    </Col>
+                    <Col sm={12} md={5} lg={5} xl={5} xxl={5}>
+                      <figure>
+                        <img src={img} />
+                      </figure>
+                    </Col>
+                  </Row>
+                </div>
+              );
+            })}
         </div>
       </Container>
     </div>
--- pages/api/project.ts
+++ pages/api/project.ts
@@ -10,6 +10,7 @@ interface Project {
   techStack: Array<string>;
   describe: string | undefined;
   image: Array<string>;
+  sort: number;
 }
 
 export type Projects = Array<Project>;
@@ -26,6 +27,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["typescript", "nextjs", "tailwindcss"],
         describe: "사실이 아닙니다! 이미지를 만들어주는 사이트입니다.",
         image: [],
+        sort: 5,
       },
       {
         name: "ft_linear_regression",
@@ -39,6 +41,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
           "/project/ft_linear_regression/1.png",
           "/project/ft_linear_regression/2.png",
         ],
+        sort: 4,
       },
       {
         name: "트채통",
@@ -61,6 +64,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "인터넷 방송 플랫폼인 Twitch에서 C++로 만든 IRC Client로 채팅 로그를 긁어와 분석, 통계를 제공하는 웹 사이트입니다.",
         image: ["/project/tchatong/1.png"],
+        sort: 1,
       },
       {
         name: "pong_pong_website",
@@ -72,6 +76,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "온라인 실시간 탁구 게임채팅, Dm, 친구 추가/삭제, 전적 등의 서비스를 제공하는 웹 어플리케이션입니다.",
         image: ["/project/pong_pong_website/1.gif"],
+        sort: 2,
       },
       {
         name: "ft_irc",
@@ -82,6 +87,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c++"],
         describe: "RFC 문서를 바탕으로 작성된 IRC 서버입니다.",
         image: ["/project/ft_irc/1.png"],
+        sort: 3,
       },
       {
         name: "ft_container",
@@ -92,6 +98,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c++"],
         describe: "c++의 표준 템플릿 라이브러리(STL)을 구현한 프로젝트입니다.",
         image: [],
+        sort: 6,
       },
       {
         name: "Philosophers",
@@ -103,6 +110,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "dead lock, data race 등의 문제로 유명한 식사하는 철학자 문제를 c로 구현, 해결한 프로젝트입니다.",
         image: [],
+        sort: 7,
       },
       {
         name: "ft_services",
@@ -123,6 +131,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "여러 서비스가 동작하는 컨테이너(nginx, mysql, ftps, wordpress, phpmyadmin, grafana, InfluxDB) 들을 쿠버네티스를 사용하여 관리하는 프로젝트입니다.",
         image: [],
+        sort: 8,
       },
       {
         name: "minishell",
@@ -133,6 +142,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c"],
         describe: "나만의 Shell을 만드는 프로젝트 입니다.",
         image: [],
+        sort: 9,
       },
       {
         name: "libasm",
@@ -144,6 +154,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "strlen, strcpy, strdup 등의 함수를 assembly 언어로 구현한 프로젝트입니다.",
         image: [],
+        sort: 10,
       },
       {
         name: "ft_printf",
@@ -154,6 +165,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c"],
         describe: "c의 printf 함수를 구현한 프로젝트입니다.",
         image: [],
+        sort: 11,
       },
       {
         name: "libft",
@@ -165,9 +177,22 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "C library의 strdup, calloc 등 유용한 함수를 재 구현한 프로젝트입니다",
         image: [],
+        sort: 12,
       },
     ],
     "en-US": [
+      {
+        name: "not true! generator",
+        githubURL: "https://github.com/AMATEURTOSS/not_true_generator",
+        url: "https://not-true-generator.vercel.app",
+        start: new Date("2022-05-03"),
+        end: new Date("2022-05-04"),
+        techStack: ["typescript", "nextjs", "tailwindcss"],
+        describe:
+          "This is a site that creates 'not true!(korean meme)' images..",
+        image: [],
+        sort: 5,
+      },
       {
         name: "ft_linear_regression",
         githubURL: "https://github.com/AMATEURTOSS/ft_linear_regression",
@@ -181,6 +206,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
           "/project/ft_linear_regression/1.png",
           "/project/ft_linear_regression/2.png",
         ],
+        sort: 4,
       },
       {
         name: "tchatong",
@@ -202,6 +228,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           'It is a website that provides statistics by scratching the chat log from the Internet broadcasting platform "Twitch" to the IRC Client made with C++.',
         image: ["/project/tchatong/1.png"],
+        sort: 1,
       },
       {
         name: "pong_pong_website",
@@ -213,6 +240,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "It is a web application that provides online real-time table tennis game chat, Dm, add/delete friends, and transfer.",
         image: ["/project/pong_pong_website/1.gif"],
+        sort: 2,
       },
       {
         name: "ft_irc",
@@ -223,6 +251,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c++"],
         describe: "IRC server written based on RFC documentation.",
         image: ["/project/ft_irc/1.png"],
+        sort: 3,
       },
       {
         name: "ft_container",
@@ -234,6 +263,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "A project that implements the standard template library (STL) of c++.",
         image: [],
+        sort: 6,
       },
       {
         name: "Philosophers",
@@ -245,6 +275,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "It is a project that implements and solves the problem of eating philosopher in C.",
         image: [],
+        sort: 7,
       },
       {
         name: "ft_services",
@@ -265,6 +296,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "It is a project that manages containers (nginx, mysql, ftps, wordpress, phpmmyadmin, grafana, InfluxDB) where several services operate using Kubernetes.",
         image: [],
+        sort: 8,
       },
       {
         name: "minishell",
@@ -275,6 +307,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c"],
         describe: "It's a project to make my own shell.",
         image: [],
+        sort: 9,
       },
       {
         name: "libasm",
@@ -286,6 +319,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "It is a project that implements functions such as strlen, strcpy, and strdup in assembly language.",
         image: [],
+        sort: 10,
       },
       {
         name: "ft_printf",
@@ -296,6 +330,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         techStack: ["c"],
         describe: "It is a project that implements the printf function of c.",
         image: [],
+        sort: 11,
       },
       {
         name: "libft",
@@ -307,6 +342,7 @@ export const getData = async (): Promise<i18n<Projects>> => {
         describe:
           "This project is a re-implementation of useful functions such as strdup and calloc of the C Library",
         image: [],
+        sort: 12,
       },
     ],
   };
